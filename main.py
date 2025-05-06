667ab95ed959539bd68d49aff9aad528c01e1f93
Luca Frauen-Berens <LFB3@outlook.de>
2025-05-06 15:11:31 +0200

    Background and main loop

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
 clock = pygame.time.Clock()
 running = True
-
+background = pygame.image.load("sky.jpg")
+def draw():
+    # Irgendwas ist noch nicht ganz richtig
+    screen.blit(background, (0, 0))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+    draw()
