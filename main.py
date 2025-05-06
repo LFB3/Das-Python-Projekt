3d24c7a2131c276b7bfe75c8acb12674e6cee71d
Luca Frauen-Berens <LFB3@outlook.de>
2025-05-06 14:56:52 +0200

    it

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import pygame
 
+pygame.init()
+screen = pygame.display.set_mode((1920, 1080))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
