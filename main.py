81b824851b2fe93fd02a59a6f032eefbe9db49a8
LFB3 <151401002+LFB3@users.noreply.github.com>
2025-05-07 16:47:01 +0200

    Revert "Update main.py"
    This reverts commit c74cce74b4774b1f62993cc98c5dd91be0a9941b.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ clock = pygame.time.Clock()
 running = True
 background = pygame.image.load("sky.jpg")
 def draw():
-    # Irgendwas ist noch nicht ganz richtig
+    # Irgendwas ist noch nicht ganz richtig lolcat wow
     screen.blit(background, (0, 0))
 while running:
     for event in pygame.event.get():
