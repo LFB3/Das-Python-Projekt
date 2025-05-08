5e3b42c93a3789e7574249c3cf49d157bd628162
LFB3 <151401002+LFB3@users.noreply.github.com>
2025-05-08 19:04:48 +0200

    Stuff

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1920, 1080))
+screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 running = True
 background = pygame.image.load("sky.jpg")
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.velocity = [0, 0]
+
+    def gravity(self):
+        self.velocity[1] += .1
+
+
+
+        self.y = self.y - self.velocity[1]
 def draw():
-    #Broken
     screen.blit(background, (0, 0))
+Player = Player()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                pass
+            if event.key == pygame.K_RIGHT:
+                pass
+            if event.key == pygame.K_UP:
+                Player.velocity[1] += 10
+            if event.key == pygame.K_DOWN:
+                Player.velocity[1] -= 10
     draw()
+    pygame.display.update()
+pygame.display.quit()
